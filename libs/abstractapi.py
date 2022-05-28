aedc29acbacf69c202c3016ce847a42b99074c0d
kunaia <alekoxanta@gmail.com>
2022-05-28 12:20:02 +0400

    changed email validation idea and added getting holiday and geolocation info getting

--- libs/abstractapi.py
+++ libs/abstractapi.py
@@ -3,15 +3,32 @@ import requests
 
 
 def validate_email(email):
-    request_url = f'https://emailvalidation.abstractapi.com/v1/?' \
-                  f'api_key={os.getenv("EMAILVERIFICATION_ABSTRACTAPI_KEY")}&' \
-                  f'email={email}'
+    request_url = f'https://emailvalidation.abstractapi.com/v1/' \
+                  f'?api_key={os.getenv("EMAILVERIFICATION_ABSTRACTAPI_KEY")}' \
+                  f'&email={email}'
 
     response = requests.get(url=request_url)
-    quality_score = float(response.json()['quality_score'])
+    deliverable = response.json()['deliverability']
 
-    return quality_score > 0.5
+    return deliverable in ['DELIVERABLE', 'RISKY']
 
 
-if __name__ == "__main__":
-    validate_email("khantadze.aleko@kiu.edu.ge")
+def get_holiday(country, year, month, day):
+    request_url = f'https://holidays.abstractapi.com/v1/' \
+                  f'?api_key={os.getenv("HOLIDAYS_ABSTRACTAPI_KEY")}' \
+                  f'&country={country}' \
+                  f'&year={year}' \
+                  f'&month={month}' \
+                  f'&day={day}'
+    response = requests.get(url=request_url)
+    return response.json()
+
+
+def get_geolocation(ip_address):
+    request_url = f'https://ipgeolocation.abstractapi.com/v1/' \
+                  f'?api_key={os.getenv("GEOLOCATION_ABSTRACTAPI_KEY")}' \
+                  f'&ip_address={ip_address}'
+    response = requests.get(url=request_url)
+    data = response.json()
+
+    return data if data.get("country", None) else None
