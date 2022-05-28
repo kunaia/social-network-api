8a20aa419eb4940e12b9946fd85d6cb4a35ea9c0
kunaia <alekoxanta@gmail.com>
2022-05-28 12:42:39 +0400

    improved testing process

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,39 +1,43 @@
 import json
-import unittest
+from datetime import datetime
+from libs.abstractapi import get_holiday
+
 from tests.BaseCase import BaseCase
 
-name = "alex"
-surname = "khantadze"
-email = "alekoxanta@gmail.com"
-password = "12345"
+
+def register(app,
+             name="name", surname="surname",
+             email="alekoxanta@gmail.com", password="12345"):
+    payload = json.dumps({
+        "name": name,
+        "surname": surname,
+        "email": email,
+        "password": password
+    })
+    response = app.post("/register",
+                        headers={"Content-Type": "application/json"},
+                        data=payload)
+    return response
+
+
+def login(app, email='alekoxanta@gmail.com', password="12345"):
+    payload = json.dumps({
+        "email": email,
+        "password": password
+    })
+    response = app.post("/login",
+                        headers={"Content-Type": "application/json"},
+                        data=payload)
+    return response
 
 
 class RegistrationTest(BaseCase):
-    def test_successfull_registration(self):
-        payload = json.dumps({
-            "name": name,
-            "surname": surname,
-            "email": email,
-            "password": password
-        })
-
-        response = self.app.post("/register",
-                                 headers={"Content-Type": "application/json"},
-                                 data=payload)
-
+    def test_successful_registration(self):
+        response = register(self.app)
         self.assertEqual(201, response.status_code)
 
-    def test_unsuccessfull_registration(self):
-        payload = json.dumps({
-            "name": name,
-            "surname": surname,
-            "email": "somerandomthings@jasf.notvalid",
-            "password": password
-        })
-
-        response = self.app.post("/register",
-                                 headers={"Content-Type": "application/json"},
-                                 data=payload)
+    def test_unsuccessful_registration(self):
+        response = register(self.app, email="somenonvalidemail@notvalid.com")
 
         self.assertEqual(400, response.status_code)
         self.assertEqual(None, response.json.get("user", None))
@@ -41,24 +45,8 @@ class RegistrationTest(BaseCase):
 
 class LoginTest(BaseCase):
     def test_login(self):
-        payload = json.dumps({
-            "name": name,
-            "surname": surname,
-            "email": email,
-            "password": password
-        })
-
-        response = self.app.post("/register",
-                                 headers={"Content-Type": "application/json"},
-                                 data=payload)
-
-        payload = json.dumps({
-            "email": email,
-            "password": password
-        })
-        response = self.app.post("/login",
-                                 headers={"Content-Type": "application/json"},
-                                 data=payload)
+        response = register(self.app, name="random_name")
+        response = login(self.app)
 
         self.assertEqual(200, response.status_code)
-        self.assertEqual("alex", response.get_json()['user']['name'])
+        self.assertEqual("random_name", response.get_json()['user']['name'])
