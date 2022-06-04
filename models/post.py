8d9c18279fe4ca90ef38ab54ada9887cca8b4764
kunaia <alekoxanta@gmail.com>
2022-06-04 11:33:12 +0400

    added view counting attribute and developed views increasing nad like/unlike mechanism

--- models/post.py
+++ models/post.py
@@ -13,6 +13,7 @@ class Post(db.Model, BaseModel):
     creation_date = db.Column(db.DateTime(timezone=True), nullable=False)
     last_update = db.Column(db.DateTime(timezone=True))
     like_cnt = db.Column(db.Integer, default=0)
+    view_cnt = db.Column(db.Integer, default=0)
     author_fk = db.Column(db.Integer, db.ForeignKey("user.id"))
 
     viewers = db.relationship('User', secondary='user_post', backref=db.backref('viewed_posts'))
@@ -21,7 +22,29 @@ class Post(db.Model, BaseModel):
     def find_by_id(cls, _id):
         return cls.query.filter_by(id=_id).first()
 
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
     def update_body(self, new_body):
         self.body = new_body
         self.last_update = datetime.datetime.now()
         db.session.commit()
+
+    def viewed_by(self, user_id):
+        user_post = UserPost(user_id, self.id)
+        if user_post.save_to_db():
+            if self.view_cnt is None:
+                self.view_cnt = 0
+            self.view_cnt += 1
+            print(self.view_cnt)
+            db.session.commit()
+
+    def like_toggled_by(self, user_id):
+        user_post = UserPost.find_by_user_post_ids(user_id, self.id)
+        if user_post is not None:
+            user_post.toggle_liked()
+            self.like_cnt += 1 if user_post.liked else -1
+            db.session.commit()
+            return True
+        return False
