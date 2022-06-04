23eac1068d58383d1cdf5e5f310cae52e6fb7e9f
kunaia <alekoxanta@gmail.com>
2022-06-04 11:34:46 +0400

    adde post_dump processing that adds info to post_json abput user likes this post or not

--- schemas/post.py
+++ schemas/post.py
@@ -1,9 +1,10 @@
 import datetime
 
 from ma import ma
-from marshmallow import post_load
+from marshmallow import post_load, post_dump
 
 from models.post import Post
+from models.user_post import UserPost
 from schemas.user import UserSchema
 
 
@@ -20,3 +21,12 @@ class PostSchema(ma.SQLAlchemyAutoSchema):
     def post_processing(self, post, *args, **kwargs):
         post.creation_date = datetime.datetime.now()
         return post
+
+    @post_dump
+    def post_dump_processing(self, post_json, *args, **kwargs):
+        post_json['liked'] = False
+        user_id = self.context.get("user_id", None)
+        if user_id:
+            user_post = UserPost.find_by_user_post_ids(user_id, post_json['id'])
+            post_json['liked'] = (user_post and user_post.liked)
+        return post_json
