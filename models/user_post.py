c4fd8d532790c4fe1d83f2b39c3a41803d86513b
kunaia <alekoxanta@gmail.com>
2022-06-04 11:31:59 +0400

    changed attribute names and added retrieving by user and post ids, also like toggling method

--- models/user_post.py
+++ models/user_post.py
@@ -6,11 +6,19 @@ class UserPost(db.Model, BaseModel):
     __tablename__ = "user_post"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_fk = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="RESTRICT"), nullable=False)
-    post_fk = db.Column(db.Integer, db.ForeignKey("post.id", ondelete="CASCADE"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="RESTRICT"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id", ondelete="CASCADE"), nullable=False)
     liked = db.Column(db.Boolean, default=False)
-    db.UniqueConstraint(user_fk, post_fk)
+    db.UniqueConstraint(user_id, post_id)
 
     def __init__(self, user_id, post_id):
-        self.user_fk = user_id
-        self.post_fk = post_id
+        self.user_id = user_id
+        self.post_id = post_id
+
+    @classmethod
+    def find_by_user_post_ids(cls, user_id, post_id):
+        return cls.query.filter_by(user_id=user_id, post_id=post_id).first()
+
+    def toggle_liked(self):
+        self.liked = not self.liked
+        db.session.commit()
